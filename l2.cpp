2342da5976741c3ad382ff6e4b04612092232fef
zuhaibarshad <120156996+zuhaibarshad-786@users.noreply.github.com>
2022-12-19 19:16:12 -0800

    ok1

---
+++ l2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main(){
+
+cout<<"        o     ^____^   "<<endl;
+
+cout<<"         o   ( o o ) \_________ "<<endl;
+
+cout<<"             ( ____ )\         )\/\  "<<endl;
+
+cout<<"                     | |-----W |     "<<endl;
+
+cout<<"                     | |      | |     "<<endl;
+    
+system("color 80");
+
+
+}
---
+++ l3.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+main()
+{
+
+
+ 
+                                                                                                                    
+                                                                                                                    
+                                                                                                                    
+                                                                                                                    
+                                                                                                                    
+                                                                                                                    
+cout<<"ppppp   ppppppppp     aaaaaaaaaaaaa      cccccccccccccccc   mmmmmmm    mmmmmmm     aaaaaaaaaaaaa  nnnn  nnnnnnnn "<<endl  ; 
+cout<<"p::::ppp:::::::::p    a::::::::::::a   cc:::::::::::::::c mm:::::::m  m:::::::mm   a::::::::::::a n:::nn::::::::nn  "<<endl;
+cout<<"p:::::::::::::::::p   aaaaaaaaa:::::a c:::::::::::::::::cm::::::::::mm::::::::::m  aaaaaaaaa:::::an::::::::::::::nn "<<endl;
+cout<<"pp::::::ppppp::::::p           a::::ac:::::::cccccc:::::cm::::::::::::::::::::::m           a::::ann:::::::::::::::n"<<endl;
+cout<<" p:::::p     p:::::p    aaaaaaa:::::ac::::::c     cccccccm:::::mmm::::::mmm:::::m    aaaaaaa:::::a  n:::::nnnn:::::n"<<endl;
+cout<<" p:::::p     p:::::p  aa::::::::::::ac:::::c             m::::m   m::::m   m::::m  aa::::::::::::a  n::::n    n::::n"<<endl;
+cout<<" p:::::p     p:::::p a::::aaaa::::::ac:::::c             m::::m   m::::m   m::::m a::::aaaa::::::a  n::::n    n::::nv"<<endl;
+cout<<"p:::::p    p::::::pa::::a    a:::::ac::::::c     cccccccm::::m   m::::m   m::::ma::::a    a:::::a  n::::n    n::::nv"<<endl;
+cout<<"p:::::ppppp:::::::pa::::a    a:::::ac:::::::cccccc:::::cm::::m   m::::m   m::::ma::::a    a:::::a  n::::n    n::::n"<<endl;
+cout<<" p::::::::::::::::p a:::::aaaa::::::a c:::::::::::::::::cm::::m   m::::m   m::::ma:::::aaaa::::::a  n::::n    n::::n"<<endl;
+cout<<"p::::::::::::::pp   a::::::::::aa:::a cc:::::::::::::::cm::::m   m::::m   m::::m a::::::::::aa:::a n::::n    n::::n"<<endl;
+cout<<" p::::::pppppppp      aaaaaaaaaa  aaaa   ccccccccccccccccmmmmmm   mmmmmm   mmmmmm  aaaaaaaaaa  aaaa nnnnnn    nnnnnn"<<endl;
+cout<<"p:::::p                                                                                                            "<<endl;
+cout<<" p:::::p                                                                                                            "<<endl;
+cout<<" p:::::p                                                                                                           "<<endl;
+cout<<" p:::::p                                                                                                           "<<endl;
+cout<<" p:::::p                                                                                                           "<<endl;
+cout<<" ppppppp                                                                                                          "<<endl;
+                                                                                                                    
+
+
+
+
+
+
+
+
+
+
+                                                                                                                       
+}
